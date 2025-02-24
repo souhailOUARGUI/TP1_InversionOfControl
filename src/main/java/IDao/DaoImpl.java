5485f2ebe486680b25ec23ce2e32488453644801
souhailOUARGUI <80362551+souhailOUARGUI@users.noreply.github.com>
2025-02-24 10:35:23 +0000

    Creation de IDao et son implémentation

---
+++ src/main/java/IDao/DaoImpl.java
@@ -0,0 +1,8 @@
+package IDao;
+
+public class DaoImpl  implements  IDao {
+    @Override
+    public double getData() {
+        return 0;
+    }
+}
---
+++ src/main/java/IDao/DaoImpl2.java
@@ -0,0 +1,8 @@
+package IDao;
+
+public class DaoImpl2 implements IDao {
+    @Override
+    public double getData() {
+        return 0;
+    }
+}
