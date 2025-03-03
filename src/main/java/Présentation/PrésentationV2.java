714f159b58fc8938ab89d85d6e2ec8a7cc4152b4
souhailOUARGUI <80362551+souhailOUARGUI@users.noreply.github.com>
2025-03-03 08:35:35 +0000

    Instenciation dynamique exemple 2

--- src/main/java/Présentation/PrésentationV2.java
+++ src/main/java/Présentation/PrésentationV2.java
@@ -1,9 +1,11 @@
 package Présentation;
 
 import IDao.IDao;
+import Metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class PrésentationV2 {
@@ -14,8 +16,18 @@ public class PrésentationV2 {
             String daoClassname = scn.nextLine();
             Class cDao = Class.forName(daoClassname);
             IDao dao =(IDao)  cDao.getConstructor().newInstance();
-            System.out.println(dao.getData());
+            //System.out.println(dao.getData());
 
+            String metierClassname = scn.nextLine();
+            Class cMetier = Class.forName(metierClassname);
+            IMetier metier = (IMetier) cMetier.getConstructor().newInstance();
+
+            Method setDao = cMetier.getDeclaredMethod("setDao",IDao.class);
+            //metier.setDao(dao)
+            setDao.invoke(metier,dao);
+
+
+            System.out.println("RES=" + metier.calcul());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
